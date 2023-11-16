7e5e871e85a49adbfd6bd8111ce1a9ae58687f09
TableStar <afraroad@gmail.com>
2023-11-16 16:42:58 +0700

    feat:backend login/reg,CRUD userProps(pic included)

--- models/usersproperties.js
+++ models/usersproperties.js
@@ -24,6 +24,7 @@ module.exports = (sequelize, DataTypes) => {
       profileImage: DataTypes.STRING,
       phoneNumber: DataTypes.STRING,
       creatorCompany: DataTypes.STRING,
+      refpoint: DataTypes.INTEGER,
     },
     {
       sequelize,

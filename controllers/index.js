73d3128e93b4b9d66876b8b013278881c7524127
hans-soerjady <soerjadyhans@gmail.com>
2023-11-24 15:03:20 +0700

    add transaction controller

--- controllers/index.js
+++ controllers/index.js
@@ -11,6 +11,7 @@ const postsController = require("./posts");
 const forgottenController = require("./forgotten");
 const pictureController = require("./picture");
 const citiesController = require("./cities")
+const transactionController = require("./transactions")
 module.exports = {
     eventsController,
     categoriesController,

e010ad593486fbdf8d4daecdaa21302f83e96795
TableStar <afraroad@gmail.com>
2024-10-04 01:58:04 +0700

    fix:cors again

--- index.js
+++ index.js
@@ -14,20 +14,10 @@ const corsOptions = {
   optionSuccessStatus: 200,
 };
 
-app.use(cors(corsOptions));
+app.use(cors());
 app.use(express.json());
 app.use(bearerToken());
 
-app.use(function (req, res, next) {
-  //Enabling CORS
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization"
-  );
-  next();
-});
 
 app.get("/", (req, res) => {
   return res.status(200).send("API RUNNING");
